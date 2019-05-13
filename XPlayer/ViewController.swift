3569df911fb2cc7383abcc6ad64191b7b492800f
邓锋 <704292743@qq.com>
2019-05-13 17:23:06 +0800

    init

---
+++ XPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XPlayer
+//
+//  Created by 邓锋 on 2019/5/13.
+//  Copyright © 2019 raisechestnut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
